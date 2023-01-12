ca0f522dbe2f88abcdccb2694671c9a315f8bb2f
maarten617 <66825212+maarten617@users.noreply.github.com>
2023-01-12 11:24:53 +0100

    Add files via upload

---
+++ M2-BO-Rein-Maarten-Opdracht verbeterd/app.js
@@ -0,0 +1,12 @@
+function facebook() {
+    window.open('http://www.facebook.com/nationaalgamemuseum')
+}
+function youtube(){
+    window.open("https://www.youtube.com/channel/UCnNuMO-7-TVI5ZEZwzU14Ew/featured?view_as=subscriber")
+}
+function twitter(){
+    window.open("https://twitter.com/NaVGMuseum")
+}
+function instagram(){
+    window.open("https://instagram.com/NVGMuseum")
+}
